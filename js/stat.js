c77ffba0679a68b782963f5f65a86532c7a3f327
Olga S <lillolin@gmail.com>
2017-03-30 23:55:10 +0300

    Color for 'Вы' doesn't work

--- js/stat.js
+++ js/stat.js
@@ -2,64 +2,56 @@
 
 window.renderStatistics = function (ctx, names, times) {
 
-ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-ctx.strokeRect(110, 20, 420, 270);
-ctx.fillRect(110, 20, 420, 270);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.strokeRect(110, 20, 420, 270);
+    ctx.fillRect(110, 20, 420, 270);
 
-ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-ctx.strokeRect(100, 10, 420, 270);
-ctx.fillRect(100, 10, 420, 270);
+    ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+    ctx.strokeRect(100, 10, 420, 270);
+    ctx.fillRect(100, 10, 420, 270);
 
-ctx.fillStyle = '#000';
-ctx.font = '16px PT Mono';
-ctx.fillText('Ура, вы победили!', 120, 40);
-ctx.fillText('Список результатов:', 120, 60);
+    ctx.fillStyle = '#000';
+    ctx.font = '16px PT Mono';
+    ctx.fillText('Ура, вы победили!', 120, 40);
+    ctx.fillText('Список результатов:', 120, 60);
 
 
-var max = -1;
-var maxIndex = -1;
+    var max = Math.max.apply(Math, times);
+    var histogramHeight = 150;
+    var step = histogramHeight / max;
 
-for (var i = 0; i < times.length; i++) {
-	var time = times [i];
-	if (time > max) {
-		max = time; 
-		maxIndex = i;
-	}
-}
+    var barWidth = 40;
+    var indent = 50;
+    var initialX = 150;
+    var initialY = 250;
 
-var histogramHeight = 150;
-var step = histogramHeight / max;
+    ctx.textBaseline = 'top';
 
-var barWidth = 40;
-var indent = 50;
-var initialX = 150;
-var initialY = 250;
+    var name = names[i];
 
-ctx.textBaseline = 'top';
+    var getRandomColor = function(name) {
 
-var name = names[i];
+        for (var i = 0; i < names.length; i++) {
 
-var getRandomColor  = function (name) {
+            if (name == 'Вы') {
+              var color = 'rgba(0, 0, 255, '+ Math.random()+')';
+            }
 
-for (var i = 0; i < names.length; i++) {
-    if name = 'Вы'  {
-	    return 'rgba(0, 0, 255, '+ Math.random()+')';
-    }
+            else {
+              var color = 'rgba(255, 0, 0, 1.0)';
+            }
 
-    else {
-	   return 'rgba(255, 0, 0, 1.0)';
-    }
-}
+            return (color);
+        }
+    }    
   
-for (var i = 0; i < times.length; i++) {
+    for (var i = 0; i < times.length; i++) {
 
-    ctx.fillStyle = getRandomColor(name);
-    ctx.fillRect(initialX + (barWidth+indent) * i, initialY, barWidth, - times[i] * step);
+        ctx.fillStyle = getRandomColor(name);
+        ctx.fillRect(initialX + (barWidth+indent) * i, initialY, barWidth, - times[i] * step);
 
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(parseInt(times[i]), initialX + (barWidth+indent) * i, initialY - histogramHeight - 20 );
-    ctx.fillText(names[i], initialX + (barWidth+indent) * i, initialY + 5 );
-
-}
-
+        ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+        ctx.fillText(parseInt(times[i]), initialX + (barWidth+indent) * i, initialY - histogramHeight - 20 );
+        ctx.fillText(names[i], initialX + (barWidth+indent) * i, initialY + 5 );
+    }   
 }
