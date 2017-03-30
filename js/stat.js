8e80ce7b86b4a983c8e16b47e526e5c05b65046b
Olga S <lillolin@gmail.com>
2017-03-30 12:14:14 +0300

    Add cycle - doesn'n work

--- js/stat.js
+++ js/stat.js
@@ -37,15 +37,29 @@ var initialY = 250;
 
 ctx.textBaseline = 'top';
 
+var name = names[i];
+
+var getRandomColor  = function (name) {
+
+for (var i = 0; i < names.length; i++) {
+    if name = 'Вы'  {
+	    return 'rgba(0, 0, 255, '+ Math.random()+')';
+    }
+
+    else {
+	   return 'rgba(255, 0, 0, 1.0)';
+    }
+}
+  
 for (var i = 0; i < times.length; i++) {
 
-ctx.fillStyle = 'rgba(0, 0, 255, '+ Math.random()+')';
-ctx.fillRect(initialX + (barWidth+indent) * i, initialY, barWidth, - times[i] * step);
+    ctx.fillStyle = getRandomColor(name);
+    ctx.fillRect(initialX + (barWidth+indent) * i, initialY, barWidth, - times[i] * step);
 
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillText(parseInt(times[i]), initialX + (barWidth+indent) * i, initialY - histogramHeight - 20 );
+    ctx.fillText(names[i], initialX + (barWidth+indent) * i, initialY + 5 );
 
-ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-ctx.fillText(parseInt(times[i]), initialX + (barWidth+indent) * i, initialY - histogramHeight - 20 );
-ctx.fillText(names[i], initialX + (barWidth+indent) * i, initialY + 5 );
 }
 
 }
